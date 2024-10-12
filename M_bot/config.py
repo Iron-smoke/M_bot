1f0f0901ec1d564f01670c85b0da1dbc195f11bc
Iron-smoke <myfavoritearcher@gmail.com>
2024-10-12 11:33:38 +0300

    re

---
+++ M_bot/config.py
@@ -0,0 +1,5 @@
+TOKEN= '7689301742:AAGVJO2wvYibkkjbaqthNNNw3BNLc6Ql38I'
+DB_URL='sqlite+aiosqlite:///db.sqlite3'
+#DB_URL='postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
+AI_TOKEN = 'wWQvlltvkUjX75K1pkp1EVdeA5jEE7NU'
+# Внимание! Добавьте config.py в файл .gitignore!
